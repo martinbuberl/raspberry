53dbe632456c5161dd8ed84ed0d9d5d4307d79d2
Martin Buberl <hello@martinbuberl.com>
2018-02-01 23:10:26 +0100

    Added simple pygame loop

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # encoding=utf8
 from __future__ import print_function
+import pygame
 
-print("foo")
+pygame.init()
+pygame.joystick.init()
+
+while not True:
+    for event in pygame.event.get():  # User did something
+        print(event.type)
