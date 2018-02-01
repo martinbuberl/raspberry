14659e0a3d05be7c4ba0f009452ed01240a65f23
Martin Buberl <hello@martinbuberl.com>
2018-02-01 23:29:34 +0100

    Some fixes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # encoding=utf8
 from __future__ import print_function
 import pygame
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 pygame.init()
 pygame.joystick.init()
 
-while not True:
+while True:
     for event in pygame.event.get():  # User did something
         print(event.type)
