188161624d58d3f58439f3ad1354b78cce684b58
Martin Buberl <hello@martinbuberl.com>
2018-02-02 17:26:25 +0100

    Fix

--- joystick.py
+++ joystick.py
@@ -75,43 +75,43 @@ while done==False:
     # Get count of joysticks
     joystick_count = pygame.joystick.get_count()
 
-    print("Number of joysticks: {}").format(joystick_count)
+    print("Number of joysticks: {}".format(joystick_count))
 
     # For each joystick:
     for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
 
-        print("Joystick {}").format(i)
+        print("Joystick {}".format(i))
 
         # Get the name from the OS for the controller/joystick
         name = joystick.get_name()
-        print("Joystick name: {}").format(name)
+        print("Joystick name: {}".format(name))
 
         # Usually axis run in pairs, up/down for one, and left/right for
         # the other.
         axes = joystick.get_numaxes()
-        print("Number of axes: {}").format(axes)
+        print("Number of axes: {}".format(axes))
 
         for i in range( axes ):
             axis = joystick.get_axis( i )
-            print("Axis {} value: {:>6.3f}").format(i, axis)
+            print("Axis {} value: {:>6.3f}".format(i, axis))
 
         buttons = joystick.get_numbuttons()
-        print("Number of buttons: {}").format(buttons)
+        print("Number of buttons: {}".format(buttons))
 
         for i in range( buttons ):
             button = joystick.get_button( i )
-            print("Button {:>2} value: {}").format(i,button)
+            print("Button {:>2} value: {}".format(i,button))
 
         # Hat switch. All or nothing for direction, not like joysticks.
         # Value comes back in an array.
         hats = joystick.get_numhats()
-        print("Number of hats: {}").format(hats)
+        print("Number of hats: {}".format(hats))
 
         for i in range( hats ):
             hat = joystick.get_hat( i )
-            print("Hat {} value: {}").format(i, str(hat))
+            print("Hat {} value: {}".format(i, str(hat)))
 
     # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
 
