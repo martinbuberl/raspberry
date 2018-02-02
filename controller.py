2ae0961266bd7568b71ab5872b7c97a059b109f8
Martin Buberl <hello@martinbuberl.com>
2018-02-03 00:04:48 +0100

    Changed indet

--- controller.py
+++ controller.py
@@ -1,8 +1,10 @@
 # encoding=utf8
 from __future__ import print_function
+
 import time
 import os
 import sys
+
 import pygame
 
 
@@ -25,53 +27,53 @@ BUTTON_R2 = 7
 
 
 def controller_init():
-    global JOYSTICK
+  global JOYSTICK
 
-    print("Waiting for controller... (press CTRL+C to quit)")
-    os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
-    pygame.init()
+  print("Waiting for controller... (press CTRL+C to quit)")
+  os.environ["SDL_VIDEODRIVER"] = "dummy" # Removes the need to have a GUI window
+  pygame.init()
 
-    while True:
-        try:
-            try:
-                pygame.joystick.init()
-                # Attempt to setup the joystick
-                if pygame.joystick.get_count() < 1:
-                    # No joystick attached, toggle the LED
-                    pygame.joystick.quit()
-                    time.sleep(0.1)
-                else:
-                    # We have a joystick, attempt to initialise it!
-                    JOYSTICK = pygame.joystick.Joystick(0)
-                    break
-            except pygame.error:
-                # Failed to connect to the joystick, toggle the LED
-                pygame.joystick.quit()
-                time.sleep(0.1)
-        except KeyboardInterrupt:
-            sys.exit()
-    print("Controller found: {}".format(JOYSTICK.get_name()))
-    JOYSTICK.init()
+  while True:
+    try:
+      try:
+        pygame.joystick.init()
+        # Attempt to setup the joystick
+        if pygame.joystick.get_count() < 1:
+          # No joystick attached, toggle the LED
+          pygame.joystick.quit()
+          time.sleep(0.1)
+        else:
+          # We have a joystick, attempt to initialise it!
+          JOYSTICK = pygame.joystick.Joystick(0)
+          break
+      except pygame.error:
+        # Failed to connect to the joystick, toggle the LED
+        pygame.joystick.quit()
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+      sys.exit()
+  print("Controller found: {}".format(JOYSTICK.get_name()))
+  JOYSTICK.init()
 
 
 def controller_events():
-    global JOYSTICK
+  global JOYSTICK
 
-    while True:
-        for e in pygame.event.get():
-            if e.type == pygame.JOYAXISMOTION:
-                axis_lstick_x = JOYSTICK.get_axis(AXIS_LSTICK_X)
-                if axis_lstick_x != 0:
-                    print("Left stick x-axis={:>6.3f}".format(axis_lstick_x))
+  while True:
+    for e in pygame.event.get():
+      if e.type == pygame.JOYAXISMOTION:
+        axis_lstick_x = JOYSTICK.get_axis(AXIS_LSTICK_X)
+        if axis_lstick_x != 0:
+          print("Left stick x-axis={:>6.3f}".format(axis_lstick_x))
 
-                axis_r2 = JOYSTICK.get_axis(AXIS_R2)
-                if axis_r2 > -1:
-                    print("R2 axis={:>6.3f}".format(axis_r2))
+        axis_r2 = JOYSTICK.get_axis(AXIS_R2)
+        if axis_r2 > -1:
+          print("R2 axis={:>6.3f}".format(axis_r2))
 
-            elif e.type == pygame.JOYBUTTONDOWN and e.button == BUTTON_R2:
-                print("R2 pressed")
-            elif e.type == pygame.JOYBUTTONUP and e.button == BUTTON_R2:
-                print("R2 released")
+      elif e.type == pygame.JOYBUTTONDOWN and e.button == BUTTON_R2:
+        print("R2 pressed")
+      elif e.type == pygame.JOYBUTTONUP and e.button == BUTTON_R2:
+        print("R2 released")
 
 
 controller_init()
