424a2ca6fbb352cd2b9f2475401e4e5cf3df00e0
Martin Buberl <hello@martinbuberl.com>
2018-02-02 20:22:08 +0100

    Fix

--- dualshock4.py
+++ dualshock4.py
@@ -92,54 +92,53 @@ def controller_events():
                     if JOYSTICK.get_button(8):
                         print("7 button pressed")
                 elif event.type == pygame.JOYBUTTONUP:
-                    #print("Joystick button released")
-                    had_event = True
+                    print("Joystick button released")
 
-                if had_event == False:
-                    # Read axis positions (-1 to +1)
-                    if AXIS_UP_DOWN_INVERTED:
-                        up_down = -JOYSTICK.get_axis(AXIS_R2)  # release --> 1     half 0     full press --> -1
-                        throttle = up_down - 1
-                        if throttle < -1.0:
-                            throttle = -1.0
-
-                        if throttle != -1.0:
-                            print("throttle : {0} ".format(throttle))
-                    else:
-                        up_down = JOYSTICK.get_axis(AXIS_R2)
-                    if AXIS_LEFT_RIGHT_INVERTED:
-                        left_right = -JOYSTICK.get_axis(AXIS_LEFT_RIGHT)
-                        #print("left_right : {}".format(left_right))
-                    else:
-                        left_right = JOYSTICK.get_axis(AXIS_LEFT_RIGHT)
-                    # Apply steering speeds
-                    if not JOYSTICK.get_button(BUTTON_FAST_TURN):
-                        left_right *= 1
-
-                    # Determine the drive power levels
-                    if JOYSTICK.get_button(AXIS_L2): # to REVERSE the car
-                        drive_left = throttle
-                        drive_right = throttle
-                    else:                                   # to move FORWARD
-                        drive_left = -throttle
-                        drive_right = -throttle
-
-                    if left_right < -0.05:
-                        # Turning right
-                        drive_left = drive_left * (1.0 - (1.0 * left_right))
-                        drive_right = drive_right - drive_left * TURN_MULTIPLIER
-                    elif left_right > 0.05:
-                        # Turning left
-                        drive_right = drive_right * (1.0 + (1.0 * left_right))
-                        drive_left = drive_left - drive_right * TURN_MULTIPLIER
-
-                    if drive_left != 1.00 or drive_right != 1.00:
-                        print("driveL :{0:.2f} || driveR : {1:.2f} ".format(drive_left, drive_right))
-
-
-                    # Set the motors to the new speeds
-                    #ENA.value = driveLeft
-                    #ENB.value = driveRight
+#                if had_event == False:
+#                    # Read axis positions (-1 to +1)
+#                    if AXIS_UP_DOWN_INVERTED:
+#                        up_down = -JOYSTICK.get_axis(AXIS_R2)  # release --> 1     half 0     full press --> -1
+#                        throttle = up_down - 1
+#                        if throttle < -1.0:
+#                            throttle = -1.0
+#
+#                        if throttle != -1.0:
+#                            print("throttle : {0} ".format(throttle))
+#                    else:
+#                        up_down = JOYSTICK.get_axis(AXIS_R2)
+#                    if AXIS_LEFT_RIGHT_INVERTED:
+#                        left_right = -JOYSTICK.get_axis(AXIS_LEFT_RIGHT)
+#                        #print("left_right : {}".format(left_right))
+#                    else:
+#                        left_right = JOYSTICK.get_axis(AXIS_LEFT_RIGHT)
+#                    # Apply steering speeds
+#                    if not JOYSTICK.get_button(BUTTON_FAST_TURN):
+#                        left_right *= 1
+#
+#                    # Determine the drive power levels
+#                    if JOYSTICK.get_button(AXIS_L2): # to REVERSE the car
+#                        drive_left = throttle
+#                        drive_right = throttle
+#                    else:                                   # to move FORWARD
+#                        drive_left = -throttle
+#                        drive_right = -throttle
+#
+#                    if left_right < -0.05:
+#                        # Turning right
+#                        drive_left = drive_left * (1.0 - (1.0 * left_right))
+#                        drive_right = drive_right - drive_left * TURN_MULTIPLIER
+#                    elif left_right > 0.05:
+#                        # Turning left
+#                        drive_right = drive_right * (1.0 + (1.0 * left_right))
+#                        drive_left = drive_left - drive_right * TURN_MULTIPLIER
+#
+#                    if drive_left != 1.00 or drive_right != 1.00:
+#                        print("driveL :{0:.2f} || driveR : {1:.2f} ".format(drive_left, drive_right))
+#
+#
+#                    # Set the motors to the new speeds
+#                    #ENA.value = driveLeft
+#                    #ENB.value = driveRight
 
             # Wait for the interval period
             time.sleep(INTERVAL) # default = 0
